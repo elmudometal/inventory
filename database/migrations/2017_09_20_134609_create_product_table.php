aa43f8a489573e13b455adbff1e20b3d6b3f0397
Hernán Guzmán <hernan.guzman@arca.cl>
2017-09-20 17:14:32 -0300

    cambios en las migraciones y template

---
+++ database/migrations/2017_09_20_134609_create_product_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('identifier');
+            $table->integer('min');
+            $table->integer('max');
+            $table->float('price');
+            $table->integer('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+        });
+    }
+}
